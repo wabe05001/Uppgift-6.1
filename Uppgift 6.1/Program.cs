5e50ba7052b2a58ee56bc65ce9a65fabeffbdcae
Meysam03 <simon.munter@icloud.com>
2023-02-10 10:28:11 +0100

    Add project files.

---
+++ Uppgift 6.1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(addera(10, 20));
+    }
+    static int addera(int tal1, int tal2)
+    {
+        return tal1 + tal2;
+    }
+}
